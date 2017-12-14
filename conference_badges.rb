e1153d18cf9a104e472dce18482718b0ad65abd2
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-12-14 19:24:10 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+def batch_badge_creator(array)
+  array2=[]
+  array.each do |name|
+    badge="Hello, my name is #{name}."
+    array2<<badge
+  end
+  array2
+end
+def assign_rooms(array)
+  array2=[]
+  array.each_with_index.each do |name, index|
+    assignment="Hello, #{name}! You'll be assigned to room #{index+1}!"
+    array2<<assignment
+  end
+  array2
+end
+def printer(array)
+  batch_badge_creator(array).each do |name|
+    puts name
+  end
+  assign_rooms(array).each do |name|
+    puts name
+  end
+end
