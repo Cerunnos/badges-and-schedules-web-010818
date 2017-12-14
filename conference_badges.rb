b239f0fba740f366f8605ebd08e893d0d0d140e5
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-12-14 19:08:50 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,9 +2,6 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 def batch_badge_creator(array)
-  array2=array
-  while array2.length>0
-    name=array2.shift
-    return "Hello, my name is #{name}"
+  array.each do |name|
   end
 end
