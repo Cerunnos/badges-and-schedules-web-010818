7b66611dcd2fa7d6217c6330199610bb7e8db95b
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-12-14 19:19:54 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,10 +10,10 @@ def batch_badge_creator(array)
   array2
 end
 def assign_rooms(array)
-  counter=1
   array2=[]
-  array.each do |name|
-    "Hello, #{name}! You'll be assigned to room #{counter}!"
-    counter+=1
+  array.each_with_index.each do |name, index|
+    assignment="Hello, #{name}! You'll be assigned to room #{index}!"
+    array2<<assignment
   end
+  array2
 end
